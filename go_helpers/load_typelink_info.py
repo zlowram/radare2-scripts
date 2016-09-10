f8bb02f2f1944bc437824a4da957486d117d740d
zlowram <zlowram@gmail.com>
2016-09-10 11:36:56 +0200

    Fixed typelink loader
    A lot of types were being missed due to a mistaken assumption

--- go_helpers/load_typelink_info.py
+++ go_helpers/load_typelink_info.py
@@ -5,17 +5,30 @@ import sys
 import base64
 import r2pipe
 
+def loadTypeLinkTab(start, end):
+    typelink_tab = []
+    off = start
+    while off < end:
+        typelink_tab += [ int(r2.cmd("pf p4 @ "+ hex(off) + "~[1]"), 16) ]
+        off += 8
+    return typelink_tab
+    
+
 r2 = r2pipe.open()
 
 typelink_start = int(r2.cmd("iS~typelink[3]"), 16)
 typelink_sz = int(r2.cmd("iS~typelink[7]"))
 typelink_end = typelink_start + typelink_sz
 
+
 first = int(r2.cmd("pf p4 @ " + hex(typelink_start) + "~[1]"),16)
 last = int(r2.cmd("pf p4 @ " + hex(typelink_end-8) + "~[1]"),16)
 
 print "[+] Loading disassemble..."
 disasm = r2.cmdj("pDj $SS@$S")
+
+print "[+] Loading .typelink table..."
+typelink_tab = loadTypeLinkTab(typelink_start, typelink_end)
 
 print "[+] Looking for types..."
 count = 0
@@ -28,7 +41,8 @@ if disasm:
                 offset_cmt = instr["offset"]
                 rip = disasm[i+1]["offset"]
                 s = ref + rip
-                if first < s < last:
+            
+                if s in typelink_tab:
                     off = int(r2.cmd("pf p4 @ "+hex(s + 40)+"~[1]"), 16)
                     off2 = int(r2.cmd("pf p4 @"+hex(off)+"~[1]"), 16)
                     sz = r2.cmd("pf p2 @"+hex(off+8)+"~[1]")
